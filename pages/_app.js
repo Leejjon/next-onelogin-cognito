c0e78f56a3106a17b5f1361f43be984a90c3ae42
Leon Liefting <leon.liefting@persgroep.net>
2021-08-19 16:44:46 +0200

    Recreated amplify files.

--- pages/_app.js
+++ pages/_app.js
@@ -2,35 +2,35 @@ import '../styles/globals.css';
 import Amplify, { Auth } from 'aws-amplify';
 import awsconfig from '../aws-exports';
 
-const environment = process.env.NODE_ENV;
-console.log(environment);
-
-const [
-  productionRedirectSignIn,
-  localRedirectSignIn,
-] = awsconfig.oauth.redirectSignIn.split(",");
-
-const [
-  productionRedirectSignOut,
-  localRedirectSignOut,
-] = awsconfig.oauth.redirectSignOut.split(",");
-
-// const updatedAwsConfig = awsconfig;
-const updatedAwsConfig = {
-  ...awsconfig,
-  oauth: {
-    ...awsconfig.oauth,
-    redirectSignIn: environment !== "production" ? localRedirectSignIn : productionRedirectSignIn,
-    redirectSignOut: environment !== "production" ? localRedirectSignOut : productionRedirectSignOut,
-  }
-}
+// const environment = process.env.NODE_ENV;
+// console.log(environment);
+//
+// const [
+//   productionRedirectSignIn,
+//   localRedirectSignIn,
+// ] = awsconfig.oauth.redirectSignIn.split(",");
+//
+// const [
+//   productionRedirectSignOut,
+//   localRedirectSignOut,
+// ] = awsconfig.oauth.redirectSignOut.split(",");
+//
+// // const updatedAwsConfig = awsconfig;
+// const updatedAwsConfig = {
+//   ...awsconfig,
+//   oauth: {
+//     ...awsconfig.oauth,
+//     redirectSignIn: environment !== "production" ? localRedirectSignIn : productionRedirectSignIn,
+//     redirectSignOut: environment !== "production" ? localRedirectSignOut : productionRedirectSignOut,
+//   }
+// }
 
 Amplify.configure({
-  ...updatedAwsConfig,
+  ...awsconfig,
   ssr: true}
 );
 
-Auth.configure(updatedAwsConfig);
+Auth.configure(awsconfig);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
