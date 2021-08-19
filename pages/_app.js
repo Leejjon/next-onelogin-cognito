35d8c6d3f6a26a4aa95fbb04b4a59630f9f356ae
Leon Liefting <leon.liefting@persgroep.net>
2021-08-19 15:58:00 +0200

    Created the authentication part.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,36 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from '../aws-exports';
+import {env} from "eslint-config-next";
+
+const environment = process.env.NODE_ENV || 'development';
+
+const [
+  productionRedirectSignIn,
+  localRedirectSignIn,
+] = awsconfig.oauth.redirectSignIn.split(",");
+
+const [
+  productionRedirectSignOut,
+  localRedirectSignOut,
+] = awsconfig.oauth.redirectSignOut.split(",");
+
+// const updatedAwsConfig = awsconfig;
+const updatedAwsConfig = {
+  ...awsconfig,
+  oauth: {
+    ...awsconfig.oauth,
+    redirectSignIn: environment !== "production" ? localRedirectSignIn : productionRedirectSignIn,
+    redirectSignOut: environment !== "production" ? localRedirectSignOut : productionRedirectSignOut,
+  }
+}
+
+Amplify.configure({
+  ...updatedAwsConfig,
+  ssr: true}
+);
+
+Auth.configure(updatedAwsConfig);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
