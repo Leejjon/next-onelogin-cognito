0e381daed3283a6e61666c2a4922adaecabdf27e
Leon Liefting <leon.liefting@persgroep.net>
2021-08-24 16:26:33 +0200

    Ok

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
-import {API, Auth, Cache, Hub} from 'aws-amplify';
+import Amplify, {API, Auth, Cache, Hub} from 'aws-amplify';
 import {Component} from "react";
+import config from "../aws-exports.js";
+
+Amplify.configure({ ...config, ssr: true });
 
 class App extends Component {
     state = {user: null, customState: null, token: null};
@@ -22,15 +25,31 @@ class App extends Component {
         Auth.currentAuthenticatedUser()
             .then(user => {
                 // Run this after the sign-in
-                const federatedInfo = Cache.getItem('federatedInfo');
-                const { token } = federatedInfo;
-                this.setState({user: user, token: token});
+                // const federatedInfo = Cache.getItem('federatedInfo');
+                // const { token } = federatedInfo;
+                this.setState({user: user});//, token: token});
             })
-            .catch(() => console.log("Not signed in"));
+            .catch((err) => console.log("Not signed in: " + err));
     }
 
     fetchBackend() {
-
+        // const apiName = 'default';
+        // const path = '/hello';
+        // const myInit = { // OPTIONAL
+        //     headers: {}, // OPTIONAL
+        //     response: true, // OPTIONAL (return the entire Axios response object instead of only response.data)
+        //     queryStringParameters: {  // OPTIONAL
+        //         name: 'param',
+        //     },
+        // };
+        //
+        // API
+        //     .get(apiName, path, myInit)
+        //     .then(response => response.json)
+        //     .then(data => console.log("Response from hello API" + JSON.stringify(data)))
+        //     .catch(error => {
+        //         console.log("Error happening" + error);
+        //     });
         fetch('/api/hello', )
             .then(response => response.json())
             .then(data => console.log('Resonse from hello API: ' + JSON.stringify(data)));
