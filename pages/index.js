1cda7a04324dd7cae160b61d02ab9cdfbde5a4dd
Leon Liefting <leon.liefting@persgroep.net>
2021-08-24 16:47:48 +0200

    Let's try without Amplify.configure again.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import Amplify, {API, Auth, Cache, Hub} from 'aws-amplify';
+import Amplify, {Auth, Hub} from 'aws-amplify';
 import {Component} from "react";
 import config from "../aws-exports.js";
-
-Amplify.configure({ ...config, ssr: true });
+//
+// Amplify.configure({ ...config, ssr: true });
 
 class App extends Component {
     state = {user: null, customState: null, token: null};
@@ -24,32 +24,12 @@ class App extends Component {
 
         Auth.currentAuthenticatedUser()
             .then(user => {
-                // Run this after the sign-in
-                // const federatedInfo = Cache.getItem('federatedInfo');
-                // const { token } = federatedInfo;
-                this.setState({user: user});//, token: token});
+                this.setState({user: user});
             })
             .catch((err) => console.log("Not signed in: " + err));
     }
 
     fetchBackend() {
-        // const apiName = 'default';
-        // const path = '/hello';
-        // const myInit = { // OPTIONAL
-        //     headers: {}, // OPTIONAL
-        //     response: true, // OPTIONAL (return the entire Axios response object instead of only response.data)
-        //     queryStringParameters: {  // OPTIONAL
-        //         name: 'param',
-        //     },
-        // };
-        //
-        // API
-        //     .get(apiName, path, myInit)
-        //     .then(response => response.json)
-        //     .then(data => console.log("Response from hello API" + JSON.stringify(data)))
-        //     .catch(error => {
-        //         console.log("Error happening" + error);
-        //     });
         fetch('/api/hello', )
             .then(response => response.json())
             .then(data => console.log('Resonse from hello API: ' + JSON.stringify(data)));
